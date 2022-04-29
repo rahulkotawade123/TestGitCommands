325e6ec58deb47e56f49e415d87a075268b2c3dd
Rahul Kothawade <rahulkothawade99@gmail.com>
2022-04-29 20:00:13 +0530

    Program To Find List of Even Numbers

---
+++ PracticeProblem_1.java
@@ -0,0 +1,22 @@
+package new_batch;
+
+ //** Find the number of first 100 even numbers **//
+
+public class PracticeProblem_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i =0;
+		int num=100;
+			System.out.println("List of even number "+num+":");
+
+			for (i=1; i<=num; i++)
+			{
+				if(i%2 ==0)
+				{
+					System.out.println(i+"");
+	}
+	}
+}
+}
