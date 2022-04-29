e406fe810429bcb264b8010a1837f1feceb9fb72
Rahul Kothawade <rahulkothawade99@gmail.com>
2022-04-29 21:10:47 +0530

    List Of Odd Numbers

---
+++ PracticeProblem_3.java
@@ -0,0 +1,22 @@
+package new_batch;
+
+ //** Find the number of first 100 odd numbers **//
+public class PracticeProblem_3 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			int i =0;
+			int num=100;
+				System.out.println("List of odd number "+num+":");
+
+				for (i=1; i<=num; i++)
+				{
+					if(i%2 ==1)
+					{
+						System.out.println(i+"");
+		}
+		}
+	}
+	}
+
